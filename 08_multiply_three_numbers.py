2d8d9e85dcfc50f298a3cac903ba3b397c0086c2
kiteB <69155170+kiteB@users.noreply.github.com>
2020-08-04 23:00:47 +0900

    End Basic Concept

---
+++ 08_multiply_three_numbers.py
@@ -0,0 +1,9 @@
+# 코드를 작성하세요.
+def multiply_three_numbers(a, b, c):
+    print(a * b * c)
+
+
+# 테스트 코드
+multiply_three_numbers(7, 3, 5)
+multiply_three_numbers(21, 4, 9)
+multiply_three_numbers(-7, 6, 3)
